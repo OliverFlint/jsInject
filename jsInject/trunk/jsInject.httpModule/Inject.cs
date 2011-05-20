40784f859d9b9689b53723b98304bd1c304aedd8
oliverflint_cp <SND\oliverflint_cp>
2011-05-20 12:03:37 +0000

    Changed method of injection from Page.Header.Controls.Add to use Response.Filter = new customStream Added jsInjectBefore appSetting. This allow configuration of where the script will be injected (before the specified string).

--- jsInject/trunk/jsInject.httpModule/Inject.cs
+++ jsInject/trunk/jsInject.httpModule/Inject.cs
@@ -1,95 +1,89 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Web;
-using System.Web.UI;
-using System.Configuration;
 using System.IO;
-using System.Web.UI.HtmlControls;
+using System.Web;
+using System.Configuration;
 
 namespace jsInject.httpModule
 {
     public class Inject : IHttpModule
     {
-        private string _jsInjectRoot {get; set;}
+        private string _jsInjectRoot { get; set; }
         private HttpApplication _context { get; set; }
 
         public void Dispose()
         {
-            _jsInjectRoot = null;
-            _context.Dispose();
+            //
         }
 
         public void Init(HttpApplication context)
         {
-            context.PostMapRequestHandler += new EventHandler(OnPostMapRequestHandler);
+            context.BeginRequest += new EventHandler(OnBeginRequest);
         }
 
-        void OnPostMapRequestHandler(object sender, EventArgs e)
+        private void OnBeginRequest(object sender, EventArgs e)
         {
             _context = (HttpApplication)sender;
-            Page page = HttpContext.Current.CurrentHandler as Page;
-            if (page != null)
+
+            if (_context.Context.Response.ContentType == "text/html")
             {
-                page.PreRenderComplete += new EventHandler(OnPreRenderComplete);
+                _jsInjectRoot = (string)ConfigurationManager.AppSettings["jsInjectRoot"];
+                string injectBefore = (string)ConfigurationManager.AppSettings["jsInjectBefore"];
+                string _stringToInject = string.Format("{0}{1}{2}", GetFileExtensionRule(), GetFileNameRule(), GetPathRule());
+                _context.Context.Response.Filter = new jsInjectFilter(_context.Context.Response.Filter, _stringToInject, injectBefore);
             }
         }
 
-        public void OnPreRenderComplete(object sender, System.EventArgs args)
-        {
-            _jsInjectRoot = (string)ConfigurationManager.AppSettings["jsInjectRoot"];
-            injectFileExtensionRule();
-            injectFileNameRule();
-            injectPathRule();
-        }
-
-        private void injectFileExtensionRule()
+        private string GetFileExtensionRule()
         {
             string _fileExtension = VirtualPathUtility.GetExtension(_context.Request.FilePath);
             string _jsFilename = string.Format("{0}Extensions/{1}.js", _jsInjectRoot, _fileExtension);
-            if(File.Exists(_context.Request.MapPath(_jsFilename)))
+            string _returnString = GetHelperText("FileExtension", _jsFilename, _context.Request.Path);
+            if (File.Exists(_context.Request.MapPath(_jsFilename)))
             {
-                injectIntoHead(_jsFilename);
+                _returnString += string.Format("\r\n<script language='javascript' src='{0}' ></script>", _jsFilename);
             }
+            else
+            {
+                _returnString += string.Empty;
+            }
+            return _returnString;
         }
 
-        private void injectFileNameRule()
+        private string GetFileNameRule()
         {
             string _fileName = VirtualPathUtility.GetFileName(_context.Request.Path);
-            string _jsFilename = string.Format("{0}FileNames/{1}.js",_jsInjectRoot,_fileName);
+            string _jsFilename = string.Format("{0}FileNames/{1}.js", _jsInjectRoot, _fileName);
+            string _returnString = GetHelperText("FileName", _jsFilename, _context.Request.Path);
             if (File.Exists(_context.Request.MapPath(_jsFilename)))
             {
-                injectIntoHead(_jsFilename);
+                _returnString += string.Format("\r\n<script language='javascript' src='{0}' ></script>", _jsFilename);
             }
+            else
+            {
+                _returnString += string.Empty;
+            }
+            return _returnString;
         }
 
-        private void injectPathRule()
+        private string GetPathRule()
         {
             string _path = _context.Request.Path;
-            string _jsFilename = string.Format("{0}Paths{1}.js",_jsInjectRoot,_path);
+            string _jsFilename = string.Format("{0}Paths{1}.js", _jsInjectRoot, _path);
+            string _returnString = GetHelperText("Path", _jsFilename, _context.Request.Path);
             if (File.Exists(_context.Request.MapPath(_jsFilename)))
             {
-                injectIntoHead(_jsFilename);
+                _returnString += string.Format("\r\n<script language='javascript' src='{0}' ></script>", _jsFilename);
             }
+            else
+            {
+                _returnString += string.Empty;
+            }
+            return _returnString;
         }
 
-        private void injectIntoHead(string jsFileName)
+        private string GetHelperText(string rule, string jsFileName, string requestPath)
         {
-            Page page = HttpContext.Current.CurrentHandler as Page;
-            if (page != null)
-            {
-                if (page.Header != null)
-                {
-                    HtmlGenericControl Include = new HtmlGenericControl("script");
-                    Include.Attributes.Add("type", "text/javascript");
-                    Include.Attributes.Add("src", jsFileName);
-                    page.Header.Controls.Add(Include);
-                }
-                else if (!page.ClientScript.IsClientScriptIncludeRegistered(page.GetType(), jsFileName))
-                {
-                    page.ClientScript.RegisterClientScriptInclude(page.GetType(), jsFileName, jsFileName);
-                }
-            }
+            return string.Format("\r\n<!-- jsInject rule='{0}' jsFileName='{1}' requestPath='{2}' -->", rule, jsFileName, requestPath);
         }
     }
 }
---
+++ jsInject/trunk/jsInject.httpModule/jsInjectFilter.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.IO;
+
+namespace jsInject.httpModule
+{
+    class jsInjectFilter : Stream
+    {
+        private System.IO.Stream Base;
+        private string _stringToInject = "<!-- stringToInject is empty! -->";
+        private string _injectBefore = "</head>";
+
+        public jsInjectFilter(System.IO.Stream ResponseStream)
+        {
+            if ((ResponseStream == null)) {
+                throw new ArgumentNullException("ResponseStream");
+            }
+            this.Base = ResponseStream;
+        }
+
+        public jsInjectFilter(System.IO.Stream ResponseStream, string stringToInject)
+        {
+            if ((ResponseStream == null))
+            {
+                throw new ArgumentNullException("ResponseStream");
+            }
+            this.Base = ResponseStream;
+            if (!string.IsNullOrEmpty(stringToInject))
+            {
+                this._stringToInject = stringToInject.ToLower();
+            }
+        }
+
+        public jsInjectFilter(System.IO.Stream ResponseStream, string stringToInject, string injectBefore)
+        {
+            if ((ResponseStream == null))
+            {
+                throw new ArgumentNullException("ResponseStream");
+            }
+            this.Base = ResponseStream;
+            if (!string.IsNullOrEmpty(stringToInject))
+            {
+                this._stringToInject = stringToInject.ToLower();
+            }
+            if (!string.IsNullOrEmpty(injectBefore))
+            {
+                this._injectBefore = injectBefore.ToLower();
+            }
+        }
+    
+        public override int Read(byte[] buffer, int offset, int count) {
+            return this.Base.Read(buffer, offset, count);
+        }
+    
+        public override long Seek(long offset, System.IO.SeekOrigin origin) {
+            return this.Base.Seek(offset, origin);
+        }
+    
+        public override void SetLength(long value) {
+            this.Base.SetLength(value);
+        }
+    
+        public override void Write(byte[] buffer, int offset, int count) {
+            //  Get HTML code
+            string HTML = System.Text.Encoding.UTF8.GetString(buffer, offset, count);
+            //  Replace the text with something else
+            HTML = HTML.ToLower().Replace(_injectBefore, string.Format("{0}{1}", this._stringToInject, _injectBefore));
+            //  Send output
+            buffer = System.Text.Encoding.UTF8.GetBytes(HTML);
+            this.Base.Write(buffer, 0, buffer.Length);
+        }
+
+        public override bool CanRead
+        {
+            get { return true; }
+        }
+
+        public override bool CanSeek
+        {
+            get { return true; }
+        }
+
+        public override bool CanWrite
+        {
+            get { return true; }
+        }
+
+        public override void Flush()
+        {
+            this.Base.Flush();
+        }
+
+        public override long Length
+        {
+            get { return this.Base.Length; }
+        }
+
+        public override long Position
+        {
+            get
+            {
+                return this.Base.Position;
+            }
+            set
+            {
+                this.Base.Position = value;
+            }
+        }
+    }
+}
